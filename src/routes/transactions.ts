864a36ae494e12cdcb8a52743cc80e0c5f6ca55d
Gilson de Freitas <92558731+gilsondefreitas2014@users.noreply.github.com>
2023-12-29 12:29:55 -0400

    fix: comentario em droptable transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -10,7 +10,7 @@ export async function helloRoutes(app: FastifyInstance) {
     if (executar === 'S') {
       // COMANDOS DDL
       // Tabela: transactions
-      await knex.schema.dropTable('transactions')
+      // await knex.schema.dropTable('transactions')
 
       await knex.schema.createTable('transactions', (table) => {
         table.uuid('id').primary()
